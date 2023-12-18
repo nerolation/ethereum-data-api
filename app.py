aa55fb5998c45bc23dbfe069834df740544f244b
Toni Wahrstätter <51536394+nerolation@users.noreply.github.com>
2023-12-18 12:22:02 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import io
+import math
 import pandas as pd
 from cryptography.fernet import Fernet
 from fastapi import FastAPI, Depends, HTTPException, status, Request, Security, Response
@@ -7,34 +8,39 @@ from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
+from slowapi.middleware import SlowAPIMiddleware  # Importing SlowAPIMiddleware
 import asyncio
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import text
 from base64 import b64decode
+from slowapi.errors import RateLimitExceeded
+from starlette.responses import PlainTextResponse
 
 
-
-testing = False
+testing = True
 
 security = HTTPBasic()
 
 
 # Initialize FastAPI and Limiter
-app = FastAPI()
 limiter = Limiter(key_func=get_remote_address)
+app = FastAPI()
+app.state.limiter = limiter 
+app.add_middleware(SlowAPIMiddleware)
+
 
 if testing:
     try:
         with open("./config/decryption_key", "r") as file:
             os.environ["decryption_key"] = file.read().strip()
         with open("./config/database_url", "r") as file:
-            os.environ["database_url"] = file.read().strip()
+            os.environ["HEROKU_POSTGRESQL_BLUE_URL"] = file.read().strip()
     except:
         raise ValueError("Problem with ./config/ settings")
 
-database_url = os.environ.get("database_url")
-print(database_url[0:10])
+
+database_url = os.environ.get("HEROKU_POSTGRESQL_BLUE_URL").replace("postgres://", "postgresql+asyncpg://")
 key = os.environ.get("decryption_key")
 fernet = Fernet(key)
 
@@ -48,17 +54,20 @@ decrypted_data = fernet.decrypt(encrypted_data)
 # Load decrypted data into DataFrame
 df = pd.read_parquet(io.BytesIO(decrypted_data))
 df = df.set_index("username").to_dict()["password"]
-print(df)
 
 
 async def authenticate_user(username: str, password: str):
-    print(username)
-    print(df.keys())
     if username in df.keys():
         if df[username] == password:
             return True
     return False
 
+def round_down_to_nearest_ten_thousand(number):
+    return math.floor(number / 10000) * 10000
+
+@app.exception_handler(RateLimitExceeded)
+async def rate_limit_handler(request, exc):
+    return PlainTextResponse(f"Rate limit exceeded. Please slow down to {str(exc)}", status_code=429)
 
 engine = create_async_engine(database_url, echo=True)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
@@ -70,14 +79,27 @@ async def get_beaconchain_data_by_slot(day: int):
         df = pd.DataFrame(result.fetchall())
         return df.to_json(orient="records")
 
+    
+async def get_validator_by_index(index: int):
+    dataset_name = "beaconchain_validators_db"
+    async with AsyncSessionLocal() as session:
+        result = await session.execute(text(f"SELECT validator_id, pubkey, withdrawn, label, label2 FROM {dataset_name} WHERE validator_id = :index"), {"index": index})
+        row = result.fetchone()
+        if row:
+            return {
+                "validator_id": row[0],
+                "pubkey": row[1],
+                "withdrawn": row[2],
+                "label": row[3],
+                "label2": row[4]
+            }
+        return None
+
 
 @app.get("/beaconchain/{day}")
-@limiter.limit("5/minute")
-async def get_beaconchain_slot(day: int, request: Request, credentials: HTTPBasicCredentials = Depends(security)):
-    username = credentials.username
-    password = credentials.password
-
-    if not await authenticate_user(username, password):
+@limiter.limit("1/minute")
+async def get_beaconchain_slot(request: Request, day: int, credentials: HTTPBasicCredentials = Depends(security)):
+    if not await authenticate_user(credentials.username, credentials.password):
         raise HTTPException(status_code=401, detail="Invalid authentication credentials")
 
     day_data = await get_beaconchain_data_by_slot(day)
@@ -85,10 +107,18 @@ async def get_beaconchain_slot(day: int, request: Request, credentials: HTTPBasi
         raise HTTPException(status_code=404, detail="No data found for the given slot")
     return {"data": day_data}
 
-# Handling rate limit errors
-@app.exception_handler(RateLimitExceeded)
-async def rate_limit_handler(request, exc):
-    return PlainTextResponse(str(exc), status_code=429)
+
+@app.get("/validator/{index}")
+@limiter.limit("1/minute")
+async def get_validators(request: Request, index: int, credentials: HTTPBasicCredentials = Depends(security)):
+    if not await authenticate_user(credentials.username, credentials.password):
+        raise HTTPException(status_code=401, detail="Invalid authentication credentials")
+
+    validator_data = await get_validator_by_index(index)
+    if not validator_data:
+        raise HTTPException(status_code=404, detail="No data found for the given validator")
+    return {"data": validator_data}
+
 
 if __name__ == "__main__":
     import uvicorn
